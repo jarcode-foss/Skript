b4b0870f634f3cf2ccf1a4801da06333f6c6d022
Roll Fizzlebeef <bryan.terce@gmail.com>
2015-02-01 14:35:56 -0800

    Improve Scope documentation

--- src/main/java/com/w00tmast3r/skript/lang/Scope.java
+++ src/main/java/com/w00tmast3r/skript/lang/Scope.java
@@ -7,21 +7,32 @@ public interface Scope extends Traversable {
     /**
      * Returns all the children traversables of this scope.
      *
-     * @return
+     * @return a list of child traversables
      */
     List<Traversable> getChildren();
 
     /**
-     * Gets the next traversable object outside of this scope.
+     * Sets the list of children in this scope.
+     */
+    void setChildren(List<Traversable> children);
+
+    /**
+     * Returns the next traversable run outside of this scope.
      *
      * @return the next traversable
      */
-    Traversable getNextOuter();
+    List<Traversable> getNextOutside();
 
     /**
-     * Called when the last child
+     * Sets the next traversable run outside of this scope.
+     */
+    void setNextOutside(Traversable children);
+
+    /**
+     * Called when the last child of the scope is reached or when a child's {@link Traversable#walk} returns null.
+     * Returns the next traversable to execute, which may not be the next
      *
-     * @return
+     * @return the next traversable
      */
     Traversable walkOutside(Context context);
 
